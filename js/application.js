0a9eadbb46d76a0bacf8e4e834286bb6939ed50e
cyberzhg <cyberzhg@gmail.com>
2014-03-14 20:07:07 +0800

    Add auto move.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- js/application.js
+++ js/application.js
@@ -1,4 +1,36 @@
 // Wait till the browser is ready to render the game (avoids glitches)
+var game;
 window.requestAnimationFrame(function () {
-  new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  game = new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
+
+last = '';
+dir = 0;
+cnt = 0;
+
+function auto() {
+  if (game == null || typeof(game) === "undefined") {
+    return;
+  }
+  var item = document.getElementById('tile-container');
+  if (item.innerHTML == last) {
+    if (++cnt > 1) {
+      dir = 1 - dir;
+      cnt = 0;
+    }
+  }
+  last = item.innerHTML;
+  if (0 === dir) {
+    game.move(0);
+    setTimeout(function() {
+      game.move(3);
+      auto();
+    }, 100);
+  } else {
+    game.move(0);
+    setTimeout(function() {
+      game.move(1);
+      auto();
+    }, 100);
+  }
+}
