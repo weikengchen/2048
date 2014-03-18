9dd8c3d6f97e8ed2ea999eb44cad35fad4d9a2c3
cyberzhg <cyberzhg@gmail.com>
2014-03-18 12:35:05 +0800

    Add Fibonacci mode.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- js/application.js
+++ js/application.js
@@ -11,7 +11,10 @@ var cnt = 0;
 var mover = undefined;
 
 function doMovementPattern(moveType) {
-  mover = setInterval(moveType, 25);
+  if (typeof(mover) != 'undefined') {
+    clearInterval(mover);
+  }
+  mover = setInterval(moveType, 50);
 }
 
 function stopMovement() {
@@ -24,7 +27,7 @@ function corner() {
   }
   var item = document.getElementById('tile-container');
   if (item.innerHTML == last) {
-    if (++cnt > 1) {
+    if (++cnt > 0) {
       dir = 1 - dir;
       cnt = 0;
     }
@@ -32,10 +35,10 @@ function corner() {
   last = item.innerHTML;
   if (0 === dir) {
     game.move(0);
-    game.move(3);
+    setTimeout(function() {game.move(3)}, 25);
   } else {
     game.move(0);
-    game.move(1);
+    setTimeout(function() {game.move(1)}, 25);
   }
 }
 
@@ -45,7 +48,7 @@ function swing() {
   }
   var item = document.getElementById('tile-container');
   if (item.innerHTML == last) {
-    if (++cnt > 1) {
+    if (++cnt > 0) {
       dir = 1 - dir;
       cnt = 0;
     }
@@ -53,10 +56,10 @@ function swing() {
   last = item.innerHTML;
   if (0 === dir) {
     game.move(0);
-    game.move(2);
+    setTimeout(function() {game.move(2)}, 25);
   } else {
     game.move(1);
-    game.move(3);
+    setTimeout(function() {game.move(3)}, 25);
   }
 }
 
@@ -69,12 +72,109 @@ function random() {
   game.move(Math.floor(Math.random() * 4));
 }
 
-function bigInteger() {
+function alwaysTwo() {
   game.addRandomTile = function () {
     if (this.grid.cellsAvailable()) {
-      var tile = new Tile(this.grid.randomAvailableCell(), 4194304);
+      var tile = new Tile(this.grid.randomAvailableCell(), 2);
       this.grid.insertTile(tile);
     }
   };
+  game.restart();
+}
+
+function fibonacci() {
+  var fib = new Array();
+  var a = 1, b = 1;
+  fib.push(a);
+  fib.push(b);
+  while (a + b <= 2147483648) {
+    var c = a + b;
+    fib.push(c);
+    a = b;
+    b = c;
+  }  
+  game.addRandomTile = function () {
+    if (this.grid.cellsAvailable()) {
+      var tile = new Tile(this.grid.randomAvailableCell(), 1);
+      this.grid.insertTile(tile);
+    }
+  };
+  game.tileMatchesAvailable = function () {
+    var self = this;
+    var tile;
+    for (var x = 0; x < this.size; x++) {
+      for (var y = 0; y < this.size; y++) {
+        tile = this.grid.cellContent({ x: x, y: y });
+        if (tile) {
+          for (var direction = 0; direction < 4; direction++) {
+            var vector = self.getVector(direction);
+            var cell   = { x: x + vector.x, y: y + vector.y };
+            var other  = self.grid.cellContent(cell);
+            if (other) {
+              var sum = other.value + tile.value;
+              for (var i = 0; i < fib.length; ++i) {
+                if (sum === fib[i]) {
+                  return true;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return false;
+  };
+  game.move = function (direction) {
+    var self = this;
+    if (this.over || this.won) return;
+    var cell, tile;
+    var vector     = this.getVector(direction);
+    var traversals = this.buildTraversals(vector);
+    var moved      = false;
+    this.prepareTiles();
+    traversals.x.forEach(function (x) {
+      traversals.y.forEach(function (y) {
+        cell = { x: x, y: y };
+        tile = self.grid.cellContent(cell);
+        if (tile) {
+          var positions = self.findFarthestPosition(cell, vector);
+          var next      = self.grid.cellContent(positions.next);
+          if (next && !next.mergedFrom) {
+            var isMerged = false;
+            for (var i = 0; i < fib.length; ++i) {
+              if (tile.value + next.value === fib[i]) {
+                var merged = new Tile(positions.next, tile.value + next.value);
+                merged.mergedFrom = [tile, next];
+                self.grid.insertTile(merged);
+                self.grid.removeTile(tile);
+                tile.updatePosition(positions.next);
+                self.score += merged.value;
+                if (merged.value === 5702887) self.won = true;
+                isMerged = true;
+                break;
+              }
+            }
+            if (!isMerged) {
+              self.moveTile(tile, positions.farthest);
+            }
+          } else {
+            self.moveTile(tile, positions.farthest);
+          }
+          if (!self.positionsEqual(cell, tile)) {
+            moved = true; 
+          }
+        }
+      });
+    });
+    if (moved) {
+      this.addRandomTile();
+      if (!this.movesAvailable()) {
+        this.over = true; 
+      }
+      this.actuate();
+    }
+  };
+  game.inputManager.events["move"] = [];
+  game.inputManager.on("move", game.move.bind(game));
   game.restart();
 }
