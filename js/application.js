9e56097afe5596818ae897a7c5120159c0fef90a
cyberzhg <cyberzhg@gmail.com>
2014-03-18 15:51:08 +0800

    Merge any and power 2.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- js/application.js
+++ js/application.js
@@ -54,10 +54,10 @@ function corner() {
   last = item.innerHTML;
   if (0 === dir) {
     game.move(0);
-    setTimeout(function() {game.move(3)}, 25);
+    setTimeout(function() {game.move(3)}, 20);
   } else {
     game.move(0);
-    setTimeout(function() {game.move(1)}, 25);
+    setTimeout(function() {game.move(1)}, 20);
   }
 }
 
@@ -75,10 +75,10 @@ function swing() {
   last = item.innerHTML;
   if (0 === dir) {
     game.move(0);
-    setTimeout(function() {game.move(2)}, 25);
+    setTimeout(function() {game.move(2)}, 20);
   } else {
     game.move(1);
-    setTimeout(function() {game.move(3)}, 25);
+    setTimeout(function() {game.move(3)}, 20);
   }
 }
 
@@ -255,6 +255,159 @@ function threes() {
             tile.updatePosition(positions.next);
             self.score += merged.value;
             if (merged.value === 1610612736) self.won = true;
+          } else {
+            self.moveTile(tile, positions.farthest);
+          }
+          if (!self.positionsEqual(cell, tile)) {
+            moved = true; 
+          }
+        }
+      });
+    });
+    if (moved) {
+      this.addRandomTile();
+      if (!this.movesAvailable()) {
+        this.over = true; 
+      }
+      this.actuate();
+    }
+  };
+  game.inputManager.events["move"] = [];
+  game.inputManager.on("move", game.move.bind(game));
+  game.restart();
+}
+
+function mergeAny() {
+  game.addRandomTile = function () {
+    if (this.grid.cellsAvailable()) {
+      var value = Math.random() < 0.5 ? 1 : 2;
+      var tile = new Tile(this.grid.randomAvailableCell(), value);
+      this.grid.insertTile(tile);
+    }
+  };
+  game.tileMatchesAvailable = function () {
+    var self = this;
+    var tile;
+    for (var x = 0; x < this.size; x++) {
+      for (var y = 0; y < this.size; y++) {
+        tile = this.grid.cellContent({ x: x, y: y });
+        if (tile) {
+          for (var direction = 0; direction < 4; direction++) {
+            var vector = self.getVector(direction);
+            var cell   = { x: x + vector.x, y: y + vector.y };
+            var other  = self.grid.cellContent(cell);
+            if (other) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  };
+  game.move = function (direction) {
+    var self = this;
+    if (this.over || this.won) return;
+    var cell, tile;
+    var vector     = this.getVector(direction);
+    var traversals = this.buildTraversals(vector);
+    var moved      = false;
+    this.prepareTiles();
+    traversals.x.forEach(function (x) {
+      traversals.y.forEach(function (y) {
+        cell = { x: x, y: y };
+        tile = self.grid.cellContent(cell);
+        if (tile) {
+          var positions = self.findFarthestPosition(cell, vector);
+          var next      = self.grid.cellContent(positions.next);
+          if (next && !next.mergedFrom) {
+            var merged = new Tile(positions.next, tile.value + next.value);
+            merged.mergedFrom = [tile, next];
+            self.grid.insertTile(merged);
+            self.grid.removeTile(tile);
+            tile.updatePosition(positions.next);
+            self.score += merged.value;
+          } else {
+            self.moveTile(tile, positions.farthest);
+          }
+          if (!self.positionsEqual(cell, tile)) {
+            moved = true; 
+          }
+        }
+      });
+    });
+    if (moved) {
+      this.addRandomTile();
+      if (!this.movesAvailable()) {
+        this.over = true; 
+      }
+      this.actuate();
+    }
+  };
+  game.inputManager.events["move"] = [];
+  game.inputManager.on("move", game.move.bind(game));
+  game.restart();
+}
+
+function powerTwo() {
+  var index = 0;
+  game.addRandomTile = function () {
+    if (this.grid.cellsAvailable()) {
+      var value = index === 0 ? 1 : index;
+      if (index == 0) {
+        index = 1;
+      } else {
+        index <<= 1;
+        if (index > 65536) {
+          index = 0;
+        }
+      }
+      var tile = new Tile(this.grid.randomAvailableCell(), value);
+      this.grid.insertTile(tile);
+    }
+  };
+  game.tileMatchesAvailable = function () {
+    var self = this;
+    var tile;
+    for (var x = 0; x < this.size; x++) {
+      for (var y = 0; y < this.size; y++) {
+        tile = this.grid.cellContent({ x: x, y: y });
+        if (tile) {
+          for (var direction = 0; direction < 4; direction++) {
+            var vector = self.getVector(direction);
+            var cell   = { x: x + vector.x, y: y + vector.y };
+            var other  = self.grid.cellContent(cell);
+            if (other && tile.value === other.value) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+    return false;
+  };
+  game.move = function (direction) {
+    var self = this;
+    if (this.over || this.won) return;
+    var cell, tile;
+    var vector     = this.getVector(direction);
+    var traversals = this.buildTraversals(vector);
+    var moved      = false;
+    this.prepareTiles();
+    traversals.x.forEach(function (x) {
+      traversals.y.forEach(function (y) {
+        cell = { x: x, y: y };
+        tile = self.grid.cellContent(cell);
+        if (tile) {
+          var positions = self.findFarthestPosition(cell, vector);
+          var next      = self.grid.cellContent(positions.next);
+          if (next && !next.mergedFrom && tile.value === next.value) {
+            var merged = new Tile(positions.next, tile.value + next.value);
+            merged.mergedFrom = [tile, next];
+            self.grid.insertTile(merged);
+            self.grid.removeTile(tile);
+            tile.updatePosition(positions.next);
+            self.score += merged.value;
           } else {
             self.moveTile(tile, positions.farthest);
           }
