3d63548c47bc56419457649aca9a007fa6cba3b7
cyberzhg <cyberzhg@gmail.com>
2014-03-18 13:13:06 +0800

    Size can be selected now.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- js/application.js
+++ js/application.js
@@ -1,7 +1,26 @@
-// Wait till the browser is ready to render the game (avoids glitches)
+function getSize() { 
+  var reg = new RegExp("(^|&)size=([^&]*)(&|$)", "i");
+  var r = location.search.substr(1).match(reg);
+  if (r != null) {
+    return parseInt(unescape(decodeURI(r[2])));
+  }
+  return 8; 
+}
+
 var game;
 window.requestAnimationFrame(function () {
-  game = new GameManager(8, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+  var size = getSize();
+  var container = document.getElementById('grid-container');
+  var html = '';
+  for (var i = 0; i < size; ++i) {
+    html += '<div class="grid-row">';
+    for (var j = 0; j < size; ++j) {
+      html += '<div class="grid-cell"></div>';
+    }
+    html += '</div>';
+  }
+  container.innerHTML = html;
+  game = new GameManager(size, KeyboardInputManager, HTMLActuator, LocalScoreManager);
 });
 
 var last = '';
