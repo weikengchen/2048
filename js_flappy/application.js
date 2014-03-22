5701827fc498d5636d4480ba6119235d43eb6af0
cyberzhg <cyberzhg@gmail.com>
2014-03-22 21:14:11 +0800

    Change interval.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- js_flappy/application.js
+++ js_flappy/application.js
@@ -2,39 +2,43 @@
 window.requestAnimationFrame(function () {
   var game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
   var cnt = 0;
-  setInterval(function() {
+  function jumper() {
     if (cnt > 0) {
       --cnt;
-      return;
-    }
-    var birdTop = parseFloat(game.actuator.birdobj.style.top);
-    var blockATop = parseFloat(game.actuator.blockobja.style.top);
-    var blockBTop = parseFloat(game.actuator.blockobjb.style.top);
-    var blockCTop = parseFloat(game.actuator.blockobjc.style.top);
-    var blockDTop = parseFloat(game.actuator.blockobjd.style.top);
-    var blockALeft = parseFloat(game.actuator.blockobja.style.left);
-    var type1 = blockATop < 180 ? (blockBTop < 180 ? 0 : 1) : 2;
-    var type2 = blockCTop < 180 ? (blockDTop < 180 ? 0 : 1) : 2;
-    if (blockALeft < -72) {
-      type1 = type2;
-    }
-    var flag = false;
-    if (type1 === 0) {
-      if (birdTop > 355) {
-        flag = true;
-      }
-    } else if (type1 === 1) {
-      if (birdTop > 220) {
-        flag = true;
-      }
     } else {
-      if (birdTop > 100) {
-        flag = true;
+      var birdTop = parseFloat(game.actuator.birdobj.style.top);
+      var blockATop = parseFloat(game.actuator.blockobja.style.top);
+      var blockBTop = parseFloat(game.actuator.blockobjb.style.top);
+      var blockCTop = parseFloat(game.actuator.blockobjc.style.top);
+      var blockDTop = parseFloat(game.actuator.blockobjd.style.top);
+      var blockALeft = parseFloat(game.actuator.blockobja.style.left);
+      var type1 = blockATop < 180 ? (blockBTop < 180 ? 0 : 1) : 2;
+      var type2 = blockCTop < 180 ? (blockDTop < 180 ? 0 : 1) : 2;
+      if (blockALeft < -72) {
+        type1 = type2;
+      }
+      var flag = false;
+      if (type1 === 0) {
+        if (birdTop > 355) {
+          flag = true;
+        }
+      } else if (type1 === 1) {
+        if (birdTop > 220) {
+          flag = true;
+        }
+      } else {
+        if (birdTop > 100) {
+          flag = true;
+        }
+      }
+      if (flag) {
+        game.jump();
+        cnt = 1;
       }
     }
-    if (flag) {
-      game.jump();
-      cnt = 1;
-    }
-  }, 50);
+    setTimeout(function() {
+      jumper();
+    }, 384 / Math.sqrt(game.score + 256));
+  }
+  jumper();
 });
